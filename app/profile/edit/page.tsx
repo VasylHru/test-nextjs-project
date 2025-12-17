558e90007d5a8644b9c052ef7ca1ba474aa1a2e7
Vasyl Hrubyi <ronaldomen7@gmail.com>
2025-12-17 17:15:40 +0200

    add-client-api

--- app/profile/edit/page.tsx
+++ app/profile/edit/page.tsx
@@ -1,14 +1,44 @@
+// app/(private routes)/profile/edit/page.tsx
 
-import { Metadata } from 'next';
+'use client'
 
-export const metadata: Metadata = {
-  title: 'Edit Profile',
-  description: 'Edit your user details and settings',
-};
+import { useEffect, useState } from 'react';
+import AvatarPicker from '@/components/AvatarPicker/AvatarPicker';
+import { getMe, updateMe } from '@/lib/clientApi';
 
 const EditProfile = () => {
-  return <div>EditProfile</div>;
-};
+  const [userName, setUserName] = useState('');
+  const [photoUrl, setPhotoUrl] = useState('');
+
+	useEffect(() => {
+    getMe().then((user) => {
+      setUserName(user.userName ?? '');
+      setPhotoUrl(user.photoUrl ?? '');
+    });
+  }, []);
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setUserName(event.target.value);
+  };
+
+  const handleSaveUser = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    await updateMe({ userName, photoUrl });
+  };
+
+  return (
+    <div>
+      <h1>Edit profile</h1>
+      <br />
+      <AvatarPicker profilePhotoUrl={photoUrl} />
+      <br />
+      <form onSubmit={handleSaveUser}>
+        <input type='text' value={userName} onChange={handleChange} />
+        <br />
+        <button type='submit'>Save user</button>
+      </form>
+    </div>
+  )
+}
 
 export default EditProfile;
-
---
+++ lib/clientApi.ts
@@ -0,0 +1,117 @@
+import  {nextServer} from "./../lib/api";
+import axios from "axios";
+
+export type Note = {
+  id: string;
+  title: string;
+  content: string;
+  categoryId: string;
+  userId: string;
+  createdAt: string;
+  updatedAt: string;
+};
+
+export type NoteListResponse = {
+  notes: Note[];
+  total: number;
+};
+
+const nextServer = axios.create({
+  baseURL: 'http://localhost:3000/api',
+  withCredentials: true, 
+});
+
+export const getNotes = async (categoryId?: string) => {
+  const res = await nextServer.get<NoteListResponse>('/notes', {
+    params: { categoryId },
+  });
+  return res.data;
+};
+
+export const getSingleNote = async (id: string) => {
+  const res = await axios.get<Note>(`/notes/${id}`);
+  return res.data;
+};
+
+export type Category = {
+  id: string;
+  name: string;
+  description: string;
+  createdAt: string;
+  updatedAt: string;
+};
+
+export const getCategories = async () => {
+  const res = await axios<Category[]>("/categories");
+  return res.data;
+};
+
+export type NewNoteData = {
+  title: string;
+  content: string;
+  categoryId: string;
+};
+
+export const createNote = async (data: NewNoteData) => {
+  const res = await axios.post<Note>('/notes', data);
+  return res.data;
+};
+
+export type RegisterRequest = {
+  email: string;
+  password: string;
+  userName: string;
+};
+
+export type User = {
+  id: string;
+  email: string;
+  userName?: string;
+  photoUrl?: string;
+  createdAt: Date;
+  updatedAt: Date;
+};
+
+export const register = async (data: RegisterRequest) => {
+  const res = await nextServer.post<User>('/auth/register', data);
+  return res.data;
+};
+
+
+export type LoginRequest = {
+  email: string;
+  password: string;
+};
+
+export const login = async (data: LoginRequest) => {
+  const res = await nextServer.post<User>('/auth/login', data);
+  return res.data;
+};
+
+type CheckSessionRequest = {
+  success: boolean;
+};
+
+export const checkSession = async () => {
+  const res = await nextServer.get<CheckSessionRequest>('/auth/session');
+  return res.data.success;
+};
+
+export const getMe = async () => {
+  const { data } = await nextServer.get<User>('/auth/me');
+  return data;
+};
+
+export const logout = async (): Promise<void> => {
+  await nextServer.post('/auth/logout')
+};
+
+export type UpdateUserRequest = {
+  userName?: string;
+  photoUrl?: string;
+};
+
+export const updateMe = async (payload: UpdateUserRequest) => {
+  const res = await nextServer.put<User>('/auth/me', payload);
+  return res.data;
+};
